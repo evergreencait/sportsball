f67fbaf126d1a19715659dc0c8508670f4597ab3
Caitlin Hines and Keely Glenn <caitlin+keely@>
2017-04-18 16:27:36 -0700

    final site

--- src/SportsBall/Startup.cs
+++ src/SportsBall/Startup.cs
@@ -44,7 +44,7 @@ namespace SportsBall
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
 
-
+            app.UseStaticFiles();
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
