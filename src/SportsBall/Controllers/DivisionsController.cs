e1c2ca2bda3bbbd794fe2f79411321ac6e3e8cce
Caitlin Hines and Keely Glenn <caitlin+keely@>
2017-04-18 15:01:56 -0700

    add teams to division details

--- src/SportsBall/Controllers/DivisionsController.cs
+++ src/SportsBall/Controllers/DivisionsController.cs
@@ -6,61 +6,59 @@ using SportsBall.Models;
 
 namespace SportsBall.Controllers
 {
-    public class TeamsController : Controller
+    public class DivisionsController : Controller
     {
         private SportsBallContext db = new SportsBallContext();
         public IActionResult Index()
         {
-            //return View(db.Teams.ToList());
-            return View(db.Teams.Include(teams => teams.Division).ToList());
+            return View(db.Divisions.ToList());
         }
 
         public IActionResult Details(int id)
         {
-            var thisTeam = db.Teams.FirstOrDefault(teams => teams.TeamId == id);
-            return View(thisTeam);
+            var thisDivision = db.Divisions.Include(divisions => divisions.Teams).FirstOrDefault(divisions =>
+            divisions.DivisionId == id);
+            return View(thisDivision);
         }
 
         public IActionResult Create()
         {
-            ViewBag.DivisionId = new SelectList(db.Divisions, "DivisionId", "Name", "Skill", "Max");
             return View();
         }
 
         [HttpPost]
-        public IActionResult Create(Team team)
+        public IActionResult Create(Division division)
         {
-            db.Teams.Add(team);
+            db.Divisions.Add(division);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
 
         public IActionResult Edit(int id)
         {
-            var thisTeam = db.Teams.FirstOrDefault(teams => teams.TeamId == id);
-            ViewBag.DivisionId = new SelectList(db.Divisions, "DivisionId", "Name", "Skill", "Max");
-            return View(thisTeam);
+            var thisDivision = db.Divisions.FirstOrDefault(divisions => divisions.DivisionId == id);
+            return View(thisDivision);
         }
 
         [HttpPost]
-        public IActionResult Edit(Team team)
+        public IActionResult Edit(Division division)
         {
-            db.Entry(team).State = EntityState.Modified;
+            db.Entry(division).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
 
         public IActionResult Delete(int id)
         {
-            var thisTeam = db.Teams.FirstOrDefault(teams => teams.TeamId == id);
-            return View(thisTeam);
+            var thisDivision = db.Divisions.FirstOrDefault(divisions => divisions.DivisionId == id);
+            return View(thisDivision);
         }
 
         [HttpPost, ActionName("Delete")]
         public IActionResult DeleteConfirmed(int id)
         {
-            var thisTeam = db.Teams.FirstOrDefault(teams => teams.TeamId == id);
-            db.Teams.Remove(thisTeam);
+            var thisDivision = db.Divisions.FirstOrDefault(divisions => divisions.DivisionId == id);
+            db.Divisions.Remove(thisDivision);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
